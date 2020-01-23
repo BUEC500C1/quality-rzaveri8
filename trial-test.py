ae2a47b8c283d8dff34753e88eea2ff3656db153
Ruby <rzaveri@bu.edu>
2020-01-23 09:08:52 -0500

    fix-test

--- trial-test.py
+++ trial-test.py
@@ -6,10 +6,9 @@ import arab2roman
 # content of test_class.py
 class TestClass:
     def test_exist(self):
-        result = hasattr(arab2roman, 'solution')
-        assert(result, "Function does not exist!")
+        result = hasattr(int2roman, 'solution')
+        assert result, "Function does not exist!"
 
 
         # is it converting correctly?
         # if you give it wrong input - text instead of number, decimal etc.
-        
