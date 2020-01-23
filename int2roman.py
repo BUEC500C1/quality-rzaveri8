b1e6196386581c81730be6ec62c0d6f14c8fb7b6
Ruby <rzaveri@bu.edu>
2020-01-23 13:33:08 -0500

    fix-convertor

--- int2roman.py
+++ int2roman.py
@@ -3,13 +3,7 @@ import pytest
  # https://www.w3resource.com/python-exercises/class-exercises/python-class-exercise-1.php
  # user enters a number and it gets converted
  # if user does not enter a number an error pops up and it will not be accepted.
-# userIn = input('Enter an integer to convert:')
-
 #How to make sure the user enters a number (integer) - www.101computing.net
-
-
-
-
 
 
 def inputNumber(message):
@@ -28,3 +22,30 @@ def inputNumber(message):
 
 #MAIN PROGRAM STARTS HERE:
 MyNum = inputNumber("Please input integer to convert:")
+
+def int_to_Roman(number):
+    #roman_num = number
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+        ]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+        ]
+    roman_num = ''
+    i = 0
+    temp_num = number
+    while  temp_num > 0:
+        for _ in range(temp_num // val[i]):
+            roman_num += syb[i]
+            temp_num -= val[i]
+        i += 1
+    return roman_num
+
+#check_input(userIn)
+print 'The Converted Number Is:', int_to_Roman(MyNum)
--- trial-test.py
+++ trial-test.py
@@ -6,9 +6,18 @@ import int2roman
 # content of test_class.py
 class TestClass:
     def test_exist(self):
-        result = hasattr(int2roman, 'solution')
+        result = hasattr(int2roman, 'int_to_Roman')
         assert result, "Function does not exist!"
-
-
-        # is it converting correctly?
+    def test_int(self):
+        result = problem4.int_to_Roman(10)
+        self.assertTrue((isinstance(result, int)))
+    def test_neg(self):
+        result = problem4.int_to_Roman(-10)
+        self.assertFalse((isinstance(result, int)))
+    def test_str(self):
+        result = problem4.int_to_Roman(s)
+        self.assertFalse((isinstance(result, int)))
+    def test_random(self):
+        result = problem4.int_to_Roman(s8109sjn-)
+        self.assertFalse((isinstance(result, int)))           # is it converting correctly?
         # if you give it wrong input - text instead of number, decimal etc.
